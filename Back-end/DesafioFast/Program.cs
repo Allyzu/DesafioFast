1fc5f7492b8d6c7176f2f54a980393b817d2dfde
Allyzu <henriqueallyson18@gmail.com>
2025-09-27 02:12:59 -0300

    Ajusta Program.cs para usar a porta do Railway

--- Back-end/DesafioFast/Program.cs
+++ Back-end/DesafioFast/Program.cs
@@ -101,6 +101,7 @@ builder.Services.AddSingleton<TokenService>();
 
 var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 // Configure o pipeline
 if (app.Environment.IsDevelopment())
